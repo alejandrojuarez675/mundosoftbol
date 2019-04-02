f7f836f6b5638bed32b76ad286c723222e858b20
alejandro <alejandrojuarez675@gmail.com>
2019-04-02 15:24:42 -0300

    agrego pagina individual para articulos largos

--- src/app/articulo/articulo.component.ts
+++ src/app/articulo/articulo.component.ts
@@ -1,12 +1,28 @@
 import { OnInit, Component } from '@angular/core';
+import { AppService } from '../app.service';
+import { PaginaDTO } from '../app.models';
 
 @Component({
-    selector: 'app-articulo',
-    templateUrl: './articulo.component.html',
-    styleUrls: ['./articulo.component.css']
-  })
-  export class ArticuloComponent implements OnInit{
+  selector: 'app-articulo',
+  templateUrl: './articulo.component.html',
+  styleUrls: ['./articulo.component.css']
+})
+export class ArticuloComponent implements OnInit {
+  info: PaginaDTO;
+  id: number;
 
-    ngOnInit(): void {
-    }
+  constructor(
+    protected appService: AppService
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.appService.getInfo().subscribe(data => {
+      this.info = data;
+    });
+
+    this.id = parseInt(window.location.href.split('/')[window.location.href.split('/').length-1]) 
+
+  }
 }  
